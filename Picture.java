c15f2344c639137d28718bb30359938f09134db0
Sandra <72sandraloga@gmail.com>
2015-10-10 20:47:34 +0200

    Creado um metodo llamado creaCara() que al invocarlo añade al anterior el dibujo de una carita

--- Picture.java
+++ Picture.java
@@ -17,6 +17,10 @@ public class Picture
     private Circle sun;
     private Circle pradera;
     private Person persona;
+    private Circle cara;
+    private Square ojo1;
+    private Square ojo2;
+    private Circle boca;
     
  
 
@@ -69,7 +73,79 @@ public class Picture
         persona.slowMoveHorizontal(140);
         
     }  
-    
+  public void creaCara()
+     {
+        wall = new Square();
+        wall.moveHorizontal(-140);
+        wall.moveVertical(20);
+        wall.changeSize(120);
+        wall.makeVisible();
+        
+        window = new Square();
+        window.changeColor("black");
+        window.moveHorizontal(-120);
+        window.moveVertical(40);
+        window.changeSize(40);
+        window.makeVisible();
+
+        roof = new Triangle();  
+        roof.changeSize(60, 180);
+        roof.moveHorizontal(20);
+        roof.moveVertical(-60);
+        roof.makeVisible();
+
+        sun = new Circle();
+        sun.changeColor("yellow");
+        sun.moveHorizontal(100);
+        sun.moveVertical(-80);
+        sun.changeSize(80);
+        sun.makeVisible();
+        
+        pradera = new Circle();
+        pradera.changeColor("green");
+        pradera.moveHorizontal(-550);
+        pradera.moveVertical(160);
+        pradera.changeSize(1200);
+        pradera.makeVisible();
+        
+        sun.slowMoveVertical(160);
+        setBlackAndWhite();
+        pradera.changeColor("black");
+        
+        persona = new Person();
+        persona.moveHorizontal(-265);
+        persona.moveVertical(30);
+        persona.makeVisible();
+        persona.slowMoveHorizontal(140); 
+        
+        cara = new Circle();
+        cara.changeSize(80);
+        cara.moveHorizontal(-150);
+        cara.moveVertical(-70);
+        cara.makeVisible();
+        
+        ojo1 = new Square();
+        ojo1.changeSize(10);
+        ojo1.changeColor("black");
+        ojo1.moveHorizontal(-210);
+        ojo1.moveVertical(-80);
+        ojo1.makeVisible();
+        
+        ojo2 = new Square();
+        ojo2.changeSize(10);
+        ojo2.changeColor("black");
+        ojo2.moveHorizontal(-185);
+        ojo2.moveVertical(-80);
+        ojo2.makeVisible();
+        
+        boca = new Circle();
+        boca.changeColor("red");
+        boca.changeSize(20);
+        boca.moveHorizontal(-120);
+        boca.moveVertical(-20);
+        boca.makeVisible();
+    } 
+  
   public void moveSun()
   
     {
